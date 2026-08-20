a1bbf466038f97a55c4ea0852d6c0a3fd974ae4c
Josh <ggubyajoshua@gmail.com>
2026-08-20 14:49:54 +0300

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,65 @@
-import 'package:flutter/material.dart';
-import './screens/ScreenOne.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-void main() =>
-    runApp(DevicePreview(enabled: true, builder: (context) => MyApp()));
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MaterialApp(
+      title: "Manager",
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      useInheritedMediaQuery: true,
+      debugShowCheckedModeBanner: false,
+      home: FavCity(),
+    ),
+  ),
+);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FavCity extends StatefulWidget {
+  @override
+  State<FavCity> createState() => _FavCityState();
+}
+
+class _FavCityState extends State<FavCity> {
+  var name = "";
+  final _city = ["London", "Kampala", "Others"];
+  var currentCity = "Others";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "My Manager",
-      home: Scaffold(
-        appBar: AppBar(title: Text("My Manager")),
-        body: ScreenOne(),
+    return Scaffold(
+      appBar: AppBar(title: Text("Manager")),
+      body: Column(
+        children: [
+          TextField(
+            onSubmitted: (String userInput) {
+              setState(() {
+                name = userInput;
+              });
+            },
+          ),
+
+          DropdownButton<String>(
+            items: _city.map((String selectedCity) {
+              return DropdownMenuItem<String>(
+                value: selectedCity,
+                child: Text(selectedCity),
+              );
+            }).toList(),
+            onChanged: (String? newCity) {
+              setState(() {
+                currentCity = newCity ?? "Others";
+              });
+            },
+            value: currentCity,
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text('Your Favourite City is $name'),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/ScreenOne.dart
+++ lib/screens/ScreenOne.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-class ScreenOne extends StatelessWidget {
-  const ScreenOne({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(child: Center(child: Text('Screen One')));
-  }
-}
